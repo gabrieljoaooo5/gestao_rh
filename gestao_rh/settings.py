e0d0f5eefbfea684e246dcff7f2e48310b1c9459
João Gabriel Andrade <joaogabriel1598@gmail.com>
2021-11-12 11:33:58 -0300

    Implementando paginas de login e logout

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -129,6 +129,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
