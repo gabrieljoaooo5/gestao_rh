0aef9cb437022eac2413188ca7674ce09921aa68
João Gabriel Andrade <joaogabriel1598@gmail.com>
2021-11-14 10:20:26 -0300

    Crud empresa

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionario.urls')),
+    path('empresa/', include('apps.empresa.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
