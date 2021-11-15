b49412977fec9c3c41f5a854240aad246a1321ba
João Gabriel Andrade <joaogabriel1598@gmail.com>
2021-11-15 15:26:00 -0300

    Crud Funcionario

--- apps/funcionario/models.py
+++ apps/funcionario/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 from apps.departamento.models import Departamento
 from apps.empresa.models import Empresa
 
@@ -9,6 +11,9 @@ class Funcionario(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
 
     def __str__(self):
         return self.nome
